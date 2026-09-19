d7c08714f1302272904366ffaa951ec4a0dbc8a6
Kunal Jadhav <100987092+Kunaljadhav-13@users.noreply.github.com>
2026-09-19 10:39:17 +0530

    Add files via upload

---
+++ Assignment no 07/Assignment.06.py
@@ -0,0 +1,10 @@
+# F
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(i, n + 1):
+        if j == i or j == n:
+            print(j, end=" ")
+        else:
+            print("  ", end=" ")
+    print()
